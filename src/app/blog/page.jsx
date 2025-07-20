7a2c33fc6c8e4a82e1ad31079a0b3dae935ff704
loory <ece12akinci@gmail.com>
2025-07-20 19:39:51 +0200

    First Blog post added

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,13 +1,14 @@
 import Navbar from "@/components/ui/navbar";
 import AboutContent from '@/content/about.mdx';
 import Content from "@/components/ui/content";
+import DataAnalysis from "@/content/DataAnalysis.mdx";
 
 export default function Blog() {
     return (
         <main className="min-h-screen bg-white text-gray-900 p-6">
             <Navbar></Navbar>
-            <Content>
-                <AboutContent/>
+            <Content className='px-42 *:list-disc'>
+                <DataAnalysis/>
             </Content>
         </main>
     );
